d73a492fce5ae96baa3b9eab7e0de5b45ea0cf9e
Stefan Jipa <17stefanjipa@gmail.com>
2023-05-23 21:26:30 +0300

    Initial Commit

---
+++ Multithreading/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Multithreading
+//
+//  Created by Ștefan Jipa on 23.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
